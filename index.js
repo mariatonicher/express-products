16b57598b52d1e2d15b0e9178f9b8ddff4f7a59d
mariatonicher <mariatonicher@users.noreply.github.com>
2024-02-07 21:38:57 +0000

    Update index.js
    test2

--- index.js
+++ index.js
@@ -65,20 +65,36 @@ app.post("/products", (req, res) => {
 });
 
 //read
-function getProducts(title) {
-  return products.find((title) => products.title === title);
-}
+app.get("/products", (req, res) => {
+  res.json(products);
+});
 
 //update
+app.put("/products/:id", (req, res) => {
+  const id = req.params.id;
+  const updatedItem = req.body;
 
+  if (id >= 0 && id < products.length) {
+    products[id] = updatedItem;
+    res.json(updatedItem);
+  } else {
+    res.status(404).json({ error: " product not found" });
+  }
+});
 //obeter pruduto por id
-
-app.get("/products/:id", (req, res) => {});
 
 //delete products
 app.delete("/products/:id", (req, res) => {
-  products = products.filter((products) => products.id === req.params.id);
-  res.send("delete products");
+  const id = req.params.id;
+
+  if (id >= 0 && id < products.length) {
+    const deletedItem = products.splice(id, 1)[0];
+    res.json(deletedItem);
+  } else {
+    res.status(404).json({ error: "User not found" });
+  }
 });
 
-la la la
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
